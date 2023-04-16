f8c6255f47d1bc9a93526b6924599c6571ee519a
Bram <brampijper@gmail.com>
2023-04-16 11:49:41 +0200

    Date is now correctly formatted

--- gatsby-config.js
+++ gatsby-config.js
@@ -105,7 +105,7 @@ module.exports = {
                   }
                   frontmatter {
                     title
-                    date
+                    date(formatString: "MMMM DD, YYYY")
                   }
                 }
               }
